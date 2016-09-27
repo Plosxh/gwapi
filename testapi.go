d633a48a08b4401c89bf416ff928d733890ea7c1
Plosxh <pierre.charrat@gmail.com>
2016-09-27 11:07:16 +0200

    GetBankPrice
    Added getBankPrices,
    Alow you the price of each item in your bank and get every item above a
    certain profit percent

--- testapi.go
+++ testapi.go
@@ -73,14 +73,18 @@ func main() {
   //https://api.guildwars2.com/v2/tokeninfo?access_token=65D84368-DA6E-9D4A-8B6E-70C0395432961B8D9A2D-1F1E-4F28-B484-9D0DFE20DBFF
    //clef := "65D84368-DA6E-9D4A-8B6E-70C0395432961B8D9A2D-1F1E-4F28-B484-9D0DFE20DBFF"
    var choix int
-   var id int
+   var id int64
    var category int
    var count int
    var objet string
    var item_id int
    var name string
    var mesObjets string
-   fmt.Println("Choisissez : 1-Mettre à jour la Banque, 2-Voir les prix, 3-Tester getUnItem, 4 par ajouter un favori :")
+   var nb int
+   var Ids []int64
+   var monProfit int
+   var fin string
+   fmt.Println("Choisissez : 1-Mettre à jour la Banque, 2-Voir les prix, 3-Tester getUnItem, 4-par ajouter un favori, 5-getBankPrice :")
    _,err := fmt.Scanln(&choix)
    if err != nil {
      log.Fatal(err)
@@ -123,6 +127,30 @@ case 3:
   getUnItem(mesObjets)
 case 4:
   addFav()
+
+case 5:
+  fmt.Println("Choisissez un profit minimum (entrez un entier entre 0 et 100) : ")
+  _,err = fmt.Scanln(&monProfit)
+  nb=0
+  db, err := sql.Open("sqlite3", "./itemgw.db")
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer db.Close()
+
+    rows,err :=db.Query("SELECT * FROM Bank")
+    for rows.Next(){
+    err = rows.Scan(&id,&item_id,&name,&category,&count)
+    //fmt.Println("ID : ", item_id," Nom : ",name, " Category : ",category," Count : ",count)
+    Ids=append(Ids,int64(item_id))
+    if err != nil {
+      log.Fatal(err)
+      fmt.Println("arrête scanln case 5")
+    }
+    nb++
+  }
+
+  getBankPrices(Ids,monProfit)
 }
   //doEvery(10*time.Second)
   //mesItems:=getItems()
@@ -134,6 +162,9 @@ case 4:
     foo2 := price{}
     getJson("https://api.guildwars2.com/v2/commerce/prices?id=19684", &foo2)
     fmt.Println(foo2.Buys.UnitePrice)*/
+    fmt.Println("apuyer sur entrer pour fermer : ")
+    _,err = fmt.Scanln(&fin)
+
 }
 
 
@@ -148,12 +179,10 @@ func getUnItem(I string)  {
   //for i := 0; i < len(Unitems); i++ {
       fmt.Println("Achat : ",Unitems.Buys.Unit_price," Vente : ",Unitems.Sells.Unit_price," Profit : ",calcFees(Unitems.Buys.Unit_price,Unitems.Sells.Unit_price))
 
-if Unitems.Buys.Unit_price != 0 && Unitems.Sells.Unit_price != 0{
-      addCsv(Unitems)
-}
-
+  if Unitems.Buys.Unit_price != 0 && Unitems.Sells.Unit_price != 0{
+        addCsv(Unitems)
+  }
   //}
-
 
 }
 
@@ -190,9 +219,98 @@ func addFav()  {
   err = rows.Scan(&id,&item_id,&name,&category,&count)
   if err != nil {
     log.Fatal(err)
+    }
   }
+  _,err =db.Exec("INSERT INTO favori VALUES (\""+choix +"\",\""+name+"\")")
+
 }
-_,err =db.Exec("INSERT INTO favori VALUES (\""+choix +"\",\""+name+"\")")
+
+func getBankPrices(I []int64,min int)  {
+  var mesPrices []price
+  var mesPrices2 []price
+  var mesPrices3 []price
+  var profit float64
+
+  //L'api est limité à 200 items à la fois du coup on sépare les 413 items en 3
+  fmt.Println("len de I : ",len(I))
+  url := "https://api.guildwars2.com/v2/commerce/prices?ids="+strconv.FormatInt(I[0],10)
+  for i := 1; i < 199; i++ {
+    url = url +","+strconv.FormatInt(I[i],10)
+  }
+
+  url2 := "https://api.guildwars2.com/v2/commerce/prices?ids="+strconv.FormatInt(I[200],10)
+  for i := 201; i < 399; i++ {
+    url2 = url2 +","+strconv.FormatInt(I[i],10)
+  }
+
+  url3 := "https://api.guildwars2.com/v2/commerce/prices?ids="+strconv.FormatInt(I[400],10)
+  for i := 401; i < 413; i++ {
+    url3 = url3 +","+strconv.FormatInt(I[i],10)
+  }
+  //fmt.Println(url)
+
+  getJson(url,&mesPrices)
+  getJson(url2,&mesPrices2)
+  getJson(url3,&mesPrices3)
+  //fmt.Println(len(mesPrices))
+  for i := 0; i < len(mesPrices); i++ {
+    //fmt.Println(I)
+    if mesPrices[i].Buys.Unit_price == 0 && mesPrices[i].Sells.Unit_price == 0{
+          supprItem(mesPrices[i].Id)
+    }else{
+      profit =calcFees(mesPrices[i].Buys.Unit_price,mesPrices[i].Sells.Unit_price)
+
+      if profit>=float64(min){
+        fmt.Println("Nom : ",getNom(mesPrices[i].Id)," | Achat : ",mesPrices[i].Buys.Unit_price," | Vente : ",mesPrices[i].Sells.Unit_price," | Profit : ",profit)
+      }else{
+        //fmt.Println("Nom : ",getNom(mesPrices[i].Id),"a un profit de : ",profit," ce qui est trop faible.")
+      }
+    }
+  }
+
+
+  for i := 0; i < len(mesPrices2); i++ {
+    //fmt.Println(I)
+    if mesPrices2[i].Buys.Unit_price == 0 && mesPrices2[i].Sells.Unit_price == 0{
+          supprItem(mesPrices2[i].Id)
+    }else{
+      profit =calcFees(mesPrices2[i].Buys.Unit_price,mesPrices2[i].Sells.Unit_price)
+
+      if profit>=float64(min){
+        fmt.Println("Nom : ",getNom(mesPrices2[i].Id)," | Achat : ",mesPrices2[i].Buys.Unit_price," | Vente : ",mesPrices2[i].Sells.Unit_price," | Profit : ",profit)
+      }else{
+        //fmt.Println("Nom : ",getNom(mesPrices[i].Id),"a un profit de : ",profit," ce qui est trop faible.")
+      }
+    }
+  }
+
+
+  for i := 0; i < len(mesPrices3); i++ {
+    //fmt.Println(I)
+    if mesPrices3[i].Buys.Unit_price == 0 && mesPrices3[i].Sells.Unit_price == 0{
+          supprItem(mesPrices3[i].Id)
+    }else{
+      profit =calcFees(mesPrices3[i].Buys.Unit_price,mesPrices3[i].Sells.Unit_price)
+
+      if profit>=float64(min){
+        fmt.Println("Nom : ",getNom(mesPrices3[i].Id)," | Achat : ",mesPrices3[i].Buys.Unit_price," | Vente : ",mesPrices3[i].Sells.Unit_price," | Profit : ",profit)
+      }else{
+        //fmt.Println("Nom : ",getNom(mesPrices[i].Id),"a un profit de : ",profit," ce qui est trop faible.")
+      }
+    }
+  }
+
+}
+
+
+func supprItem(Id int64){
+  db, err := sql.Open("sqlite3", "./itemgw.db")
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer db.Close()
+
+    _,err =db.Exec("DELETE from bank where id="+strconv.FormatInt(Id,10))
 
 }
 
@@ -202,16 +320,16 @@ func checkBank(key string)  {
   //var tempo1 []banqueMatXml
   //var foo2 banqueMatXml
   //fmt.Println("allo ?")
-getJson("https://api.guildwars2.com/v2/account/materials?access_token="+key, &foo1)
-fmt.Println("vous avez : ",len(foo1)," objects dans vos materiaux.")
-  //fmt.Println("allo 2 ?")
+  getJson("https://api.guildwars2.com/v2/account/materials?access_token="+key, &foo1)
+  fmt.Println("vous avez : ",len(foo1)," objects dans vos materiaux.")
+    //fmt.Println("allo 2 ?")
 
-writer,_ :=os.OpenFile("./gwitem.xml", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-enc := xml.NewEncoder(writer)
-//fmt.Println("test")
-//enc.Indent("  ", "    ")
-//fmt.Println(len(foo1))
-/*for i := 0; i < len(foo1); i++ {
+  writer,_ :=os.OpenFile("./gwitem.xml", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+  enc := xml.NewEncoder(writer)
+  //fmt.Println("test")
+  //enc.Indent("  ", "    ")
+  //fmt.Println(len(foo1))
+  /*for i := 0; i < len(foo1); i++ {
   fmt.Println("bla")
 
   tempo1[i].Id = foo1[i].Id
@@ -251,8 +369,6 @@ enc := xml.NewEncoder(writer)
 
 
     }
-
-
 }
 
 func getClef()string{
@@ -268,12 +384,12 @@ func getClef()string{
 
 
 func getItems()  []items{
-url := "https://api.guildwars2.com/v2/items"
+  url := "https://api.guildwars2.com/v2/items"
 
-var mesItems []items
+  var mesItems []items
 
-getJson(url,&mesItems)
-return mesItems
+  getJson(url,&mesItems)
+  return mesItems
 
 }
 
@@ -303,18 +419,18 @@ func pingApi(t time.Time) price{
   //fmt.Printf("%s\n", string(file))
 
  jsontype := new(price)
-json.Unmarshal(file, &jsontype)
-fmt.Printf("Results: %v\n", jsontype.Id)
+  json.Unmarshal(file, &jsontype)
+  fmt.Printf("Results: %v\n", jsontype.Id)
 
-//addCsv(*foo1)
-/*d := json.NewDecoder(strings.NewReader(jsontype))
-d.UseNumber()
-var x interface{}
-if err := d.Decode(&x); err != nil {
-    log.Fatal(err)
-}
-fmt.Printf("decoded to %#v\n", x)*/
-return foo1
+  //addCsv(*foo1)
+  /*d := json.NewDecoder(strings.NewReader(jsontype))
+  d.UseNumber()
+  var x interface{}
+  if err := d.Decode(&x); err != nil {
+      log.Fatal(err)
+  }
+  fmt.Printf("decoded to %#v\n", x)*/
+  return foo1
 }
 
 func addCsv(p price) {
@@ -372,5 +488,5 @@ func getNom(id int64)string{
   var monMat []mat
   getJson("https://api.guildwars2.com/v2/items?ids="+strconv.FormatInt(id,10), &monMat)
 
-return monMat[0].Name
+  return monMat[0].Name
 }
